c2775a2abc60ce2a0c979110ad32ed30f84c2f28
xurui203 <xurui203@gmail.com>
2014-11-19 01:42:16 -0500

    created work items

---
+++ db/migrate/20141119062758_create_workitems.rb
@@ -0,0 +1,16 @@
+class CreateWorkitems < ActiveRecord::Migration
+  def change
+    create_table :workitems do |t|
+      t.text :content
+      t.references :user, index: true
+      t.datetime :due_date
+      t.boolean :active
+      t.integer :minutes_needed
+      t.integer :minutes_completed
+
+      t.timestamps
+    end
+
+    add_index :workitems, [:user_id, :created_at]
+  end
+end
---
+++ test/models/workitem_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WorkitemTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

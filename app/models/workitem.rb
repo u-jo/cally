2916cd7dbdcc2357a9eaaf0e7d10cf07be53f770
xurui203 <xurui203@gmail.com>
2014-11-19 04:34:32 -0500

    workitem controller

--- app/models/workitem.rb
+++ app/models/workitem.rb
@@ -1,7 +1,6 @@
 class Workitem < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :content, :active, :due_date, :minutes_needed, :minutes_completed
-  validates :email, presence: true
+  validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 
   default_scope -> { order('created_at') }
